719619d23908453d013dabc96370598b093ce1e0
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-07-09 15:49:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,7 @@ def load_library(file_path)
   emoticon_hash = {}
   get_meaning = {} 
   get_emoticon = {}
-  
+
   emoticons.each do |english, set|
     get_meaning[set.last] = english
     get_emoticon[set.first] = set.last
@@ -24,7 +24,7 @@ end
 def get_japanese_emoticon(file_path, emoticon)
   # code goes here
   emoticon_hash = load_library(file_path)
-  if emoticon_hash.
+  if emoticon_hash["get_emoticon"][]
 end
 
 def get_english_meaning
