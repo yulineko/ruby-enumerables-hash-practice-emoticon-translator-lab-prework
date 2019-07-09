b907076c35757ffc7578fe184302b858856f3d29
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-07-09 16:02:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -30,6 +30,7 @@ def get_japanese_emoticon(file_path, emoticon)
   if japanese_emoticon.nil? 
     return "Sorry, that emoticon was not found"
   end
+  
 end
 
 def get_english_meaning(file_path, emoticon)
