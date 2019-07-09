afc4ae53ae1b70293773a24254cfd6bbb9d6173e
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-07-09 15:22:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,7 +11,7 @@ def load_library(path)
   }
   
   emoticon_hash.each do |english_name, set|
-    emoticon_hash[:get_meaning][set]
+    emoticon_hash[:get_meaning][set.last] = 
   end
   
   emoticon_hash
