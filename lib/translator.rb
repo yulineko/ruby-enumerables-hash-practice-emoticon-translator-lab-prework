d9ade05baeba3363f1e817991a7fad2d255dcb4f
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-07-09 15:52:28 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,26 +26,9 @@ def get_japanese_emoticon(file_path, emoticon)
   emoticon_hash = load_library(file_path)
   if emoticon_hash["get_emoticon"][emoticon].nil
     return "Sorry"
-  else dictionary["get_emoticon"][emoticon]
-  end
-  
+  else emoticon_hash["get_emoticon"][emoticon]
+  en
 end
-
-def get_japanese_emoticon(library, emoticon)
-  # code goes here	  
-  dictionary = load_library(library)
-  return "Sorry, that emoticon was not found" if dictionary["get_emoticon"][emoticon].nil?
-  dictionary["get_emoticon"][emoticon]
-end	end
-
-
-
-
-
-
-
-
-
 
 def get_english_meaning
   # code goes here
