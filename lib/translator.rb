0067cf5f43d361e01f4bd1b989261885856c86df
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-07-09 15:32:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,22 +1,21 @@
-def load_library(directory)
-  # code goes here	  get_meaning, get_emoticon = {}, {}
-  YAML.load_file(directory).each do |desc, data|
-    get_meaning[data[1]] = desc
-    get_emoticon[data[0]] = data[1]
-  end
-  translations = {"get_meaning" => get_meaning, "get_emoticon" => get_emoticon}
-  translations
-end	end
+# require modules here
+require "yaml"
 
+def load_library(path)
+  # code goes here
+  emoticons = YAML.load_file(path)
+  
+  emoticon_hash = {}
+  emoticon_hash[] = {}
+  emoticon_hash[] = {}
+  
+  emoticon_hash
+end
 
- def get_japanese_emoticon	def get_japanese_emoticon(library, emoticon)
-  # code goes here	  dictionary = load_library(library)
-  return "Sorry, that emoticon was not found" if dictionary["get_emoticon"][emoticon].nil?
-  dictionary["get_emoticon"][emoticon]
-end	end
+def get_japanese_emoticon
+  # code goes here
+end
 
-
- def get_english_meaning	def get_english_meaning(library, emoticon)
-  # code goes here	  dictionary = load_library(library)
-  return "Sorry, that emoticon was not found" if dictionary["get_meaning"][emoticon].nil?
-  dictionary["get_meaning"][emoticon]
+def get_english_meaning
+  # code goes here
+end
