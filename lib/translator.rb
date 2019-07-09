34b905d5606b07077260858bc11ffb19a22af77e
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-07-09 15:24:28 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require "yaml"
 
 def load_library(path)
   # code goes here
-  emoticons = YAML.load_file('emoticons.yml')
+  emoticons = YAML.load_file(path)
   
   emoticon_hash = {
     :get_meaning => {},
